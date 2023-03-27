16c89efcf9498838015e5da5343b4fcf1438a0d2
MoritzKastner <moritzkastner@gmx.at>
2023-03-27 18:47:18 +0200

    change

---
+++ dateobject.js
@@ -0,0 +1,27 @@
+usestrict
+function currentDay(){
+    
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const days = ["Monday","Tuesday","Wednesday","Thursday","Saturday","Sunday"];   
+getFullYear()	
+getMonth()	
+getDate()
+getDay()
+let month = months[d.getMonth()];
+let day = days[d.getDay()];
+let date = getDay(day) + getDate() + getMonth(month) + getFullYear();
+return date;
+}
+function currentTime(){
+    let time = currentDay.getHours() + ":" + currentDay.getMinutes() + ":" + currentDay.getSeconds();
+    return time;
+}
+function addLeadingZero(value){
+value;
+}
+function timeOnPage(){
+
+}
+function refresh(){
+    
+}
